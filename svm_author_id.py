afb471053e2257160e1d6e9cb4ddc39844b8ead3
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2020-04-20 21:38:31 -0400

    Improved accuracy

--- svm_author_id.py
+++ svm_author_id.py
@@ -25,10 +25,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC 
 from sklearn.metrics import accuracy_score
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
-
-clf = SVC(kernel = "rbf", gamma = "auto")
+clf = SVC(kernel = "rbf", gamma = "auto", C = 10000)
 clf.fit(features_train, labels_train) 
 pred = clf.predict(features_test)
 
